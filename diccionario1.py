01530bae8674c89664507e52dc6c179960a0f25f
diego gonzalez <diego.gonzalez.berlanga2006@gmail.com>
2026-08-31 23:20:59 -0600

    diccionario1

---
+++ diccionario1.py
@@ -0,0 +1,9 @@
+estudiante = {
+    "nombre": "Carlos",
+    "edad": 19,
+    "carrera": "LTI"
+}
+
+print("Nombre:", estudiante["nombre"])
+print("Edad:", estudiante["edad"])
+print("Carrera:", estudiante["carrera"])
