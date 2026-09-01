db3a54faa4487a15fdefc14dd51d6831378ece9f
diego gonzalez <diego.gonzalez.berlanga2006@gmail.com>
2026-08-31 23:16:15 -0600

    lista3

---
+++ lista3.py
@@ -0,0 +1,9 @@
+videojuegos = ["Minecraft", "Fortnite", "Zelda", "Mario"]
+
+print("Lista original:")
+print(videojuegos)
+
+videojuegos[1] = "Overwatch"
+
+print("Lista modificada:")
+print(videojuegos)
