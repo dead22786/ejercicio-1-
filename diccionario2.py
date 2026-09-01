394c70607fa25edbc011102bea68c1058e1861bd
diego gonzalez <diego.gonzalez.berlanga2006@gmail.com>
2026-08-31 23:21:49 -0600

    diccionario2

---
+++ diccionario2.py
@@ -0,0 +1,9 @@
+producto = {
+    "nombre": "Audífonos",
+    "marca": "Sony",
+    "precio": 1200
+}
+
+print("Producto:", producto["nombre"])
+print("Marca:", producto["marca"])
+print("Precio:", producto["precio"])
