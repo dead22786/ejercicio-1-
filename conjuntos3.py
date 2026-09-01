4980a9da25226a58891e0a7f884b0bb5dbf17693
diego gonzalez <diego.gonzalez.berlanga2006@gmail.com>
2026-08-31 23:25:56 -0600

    conjuntos3

---
+++ conjuntos3.py
@@ -0,0 +1,13 @@
+grupo1 = {"Ana", "Luis", "Carlos"}
+grupo2 = {"Carlos", "María", "Pedro"}
+
+todos = grupo1.union(grupo2)
+
+print("Grupo 1:")
+print(grupo1)
+
+print("Grupo 2:")
+print(grupo2)
+
+print("Todos los alumnos:")
+print(todos)
