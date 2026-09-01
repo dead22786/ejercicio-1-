8b867c8856a99d8fc7c0b348ef12d32600fcc30c
diego gonzalez <diego.gonzalez.berlanga2006@gmail.com>
2026-08-31 23:23:36 -0600

    diccionrio3

---
+++ diccionrio3.py
@@ -0,0 +1,13 @@
+persona = {
+    "nombre": "Luis",
+    "edad": 20,
+    "ciudad": "Monterrey"
+}
+
+print("Información original:")
+print(persona)
+
+persona["edad"] = 21
+
+print("Información actualizada:")
+print(persona)
